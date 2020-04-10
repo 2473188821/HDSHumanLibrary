ed6c373798a737e7acf41249eb152f78b82586d4
Chenfy <chenfy@bokecc.com>
2020-04-10 10:48:57 +0800

    initial commit
    Signed-off-by: Chenfy <chenfy@bokecc.com>

---
+++ HDSHumanLibrary/Classes/EatDrink.framework/Headers/DailyClient.h
@@ -0,0 +1,20 @@
+//
+//  DailyClient.h
+//  EatDrink
+//
+//  Created by Chenfy on 2020/3/12.
+//  Copyright © 2020 Chenfy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DailyClient : NSObject
+
+- (void)eat;
+- (void)drink;
+
+@end
+
+NS_ASSUME_NONNULL_END
