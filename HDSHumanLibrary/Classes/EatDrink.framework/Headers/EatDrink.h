da19b589bbec39d304f4b872c8b4e41d0ab0ee2b
Chenfy <chenfy@bokecc.com>
2020-04-10 14:12:45 +0800

    frame work uplload

---
+++ HDSHumanLibrary/Classes/EatDrink.framework/Headers/EatDrink.h
@@ -0,0 +1,20 @@
+//
+//  EatDrink.h
+//  EatDrink
+//
+//  Created by Chenfy on 2020/3/12.
+//  Copyright © 2020 Chenfy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EatDrink.
+FOUNDATION_EXPORT double EatDrinkVersionNumber;
+
+//! Project version string for EatDrink.
+FOUNDATION_EXPORT const unsigned char EatDrinkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EatDrink/PublicHeader.h>
+#import <EatDrink/DailyClient.h>
+
+
