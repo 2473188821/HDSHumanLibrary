4c6d6bf670cd5d5ebc26edd5fb63a64ef9360352
Chenfy <chenfy@bokecc.com>
2020-04-10 12:34:29 +0800

    fix

---
+++ HDSHumanLibrary/Classes/DailyClient.m
@@ -0,0 +1,23 @@
+//
+//  DailyClient.m
+//  EatDrink
+//
+//  Created by Chenfy on 2020/3/12.
+//  Copyright © 2020 Chenfy. All rights reserved.
+//
+
+#import "DailyClient.h"
+
+@implementation DailyClient
+
+- (void)eat
+{
+    NSLog(@"DailyClient-----%s---",__func__);
+}
+- (void)drink
+{
+    NSLog(@"DailyClient-----%s---",__func__);
+}
+
+
+@end
